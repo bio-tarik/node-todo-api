26d711d50bb5f7bc5d69b43d6d542fce8a7ea43c
bio-tarik <bio.tarik@gmail.com>
2018-07-16 14:05:55 -0300

    Adding travis ci support

--- server/server.js
+++ server/server.js
@@ -1,3 +1,15 @@
+const env = process.env.NODE_ENV || 'development';
+
+console.log('********', env);
+
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -9,7 +21,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.post('/todos', (req, res) => {
   const todo = new Todo({
