d929732dbabc05e8c617388d76d56ead3298cb15
bio-tarik <bio.tarik@gmail.com>
2018-10-10 09:28:45 -0300

    Hashing user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -68,6 +68,21 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.pre('save', function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = {
